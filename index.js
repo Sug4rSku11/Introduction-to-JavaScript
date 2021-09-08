877fe9f7b6baf4b83ba27c22cab2db21357abbe2
desiree <desireegarcia@mini.local>
2021-09-07 19:04:09 -0600

    completed task 4

--- index.js
+++ index.js
@@ -185,8 +185,19 @@ let computer = Math.random();
     computer = 'scissors'
   }
 function game(user, computer){
-  
+  if (user === computer){
+    return `it's a tie`;
+  } else if (user === 'rock' && computer === 'scissors') {
+    return `you win`;
+  }else if (user === 'paper' && computer === 'rock'){
+    return `you win`;
+  }else if (user === 'scissors' && computer === 'paper'){
+    return `you win`;
+  } else {
+    return `you lose`;
+  }
 }
+  console.log(game('rock', computer));
 
 
 
